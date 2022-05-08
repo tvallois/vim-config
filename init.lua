6e4e60f6a2630076d4a10a237a1b2b0eeddc6f52
Thibault VALLOIS <Thibault.vallois@protonmail.com>
2022-05-08 18:54:50 +0200

    migrate from vim script config to lua

--- init.lua
+++ init.lua
@@ -2,15 +2,13 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.vim/plugged')
 Plug('neoclide/coc.nvim', { branch = 'release' })
 Plug 'tpope/vim-surround'
-Plug 'tpope/vim-fugitive'
 Plug 'tpope/vim-commentary'
 Plug 'tpope/vim-repeat'
 Plug('dracula/vim', { as = 'dracula' })
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate'})
-Plug 'itchyny/lightline.vim'
-Plug 'mengelbrecht/lightline-bufferline'
+Plug 'nvim-lualine/lualine.nvim'
 Plug 'ryanoasis/vim-devicons'
 Plug 'TimUntersberger/neogit'
 Plug('mg979/vim-visual-multi', {branch = 'master'})
@@ -46,8 +44,8 @@ vim.api.nvim_set_keymap("n", "<leader><Left>", "<C-W><C-H>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>b", ":bprev", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>n", ":bnext", { noremap = true })
 
-vim.api.nvim_set_keymap("n", "<leader>e", ":e $MYVIMRC", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader><leader>", ":source $MYVIMRC", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>e", ":e $MYVIMRC<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader><leader>", ":source $MYVIMRC<CR>", { noremap = true })
 
 vim.api.nvim_set_keymap("v", "<C-c>", '"+yy', { noremap = true })
 
@@ -56,34 +54,34 @@ vim.api.nvim_set_keymap("t", "<Rsc>", "<C-\\><C-n>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-space>", "<Cmd>CocCommand explorer<CR>", {})
 
 
+function OpenDefInNewTab()
+  vim.fn.CocAction("jumpDefinition", "tabe")
+end
+
+function ShowDocumentation()
+  vim.fn.CocAction("doHover")
+end
+
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", { silent = true})
 vim.api.nvim_set_keymap("n", "gs", ":sp<CR><Plug>(coc-definition)", { silent = true})
-vim.api.nvim_set_keymap("n", "gt", "v.lua.OpenDefInNewTab()<CR>", { silent = true})
+vim.keymap.set("n", "gq", OpenDefInNewTab, { silent = true})
 vim.api.nvim_set_keymap("n", "gy <Plug>", "<Plug>(coc-type-definition)", { silent = true})
 vim.api.nvim_set_keymap("n", "gi <Plug>", "<Plug>(coc-implementation)", { silent = true})
 vim.api.nvim_set_keymap("n", "gr <Plug>", "<Plug>(coc-references)", { silent = true})
 vim.api.nvim_set_keymap("n", "[g <Plug>", "<Plug>(coc-diagnostic-prev)", { silent = true})
 vim.api.nvim_set_keymap("n", "]g <Plug>", "<Plug>(coc-diagnostic-next)", { silent = true})
-vim.api.nvim_set_keymap("n", "K", "v:lua.ShowDocumentation()<CR>", { silent = true})
-
-function _G.OpenDefInNewTab()
-  vim.call("CocAction('jumpDefinition', 'tabe')")
-end
-
-function _G.ShowDocumentation()
-  vim.call("CocAction('doHover')")
-end
+vim.keymap.set("n", "K", ShowDocumentation, { silent = true, noremap = true})
 
 function _G.CheckBackSpace()
   local row_col = vim.api.nvim_win_get_cursor(0)
   return vim.api.nvim_get_current_line()[row_col[1] - 1]
 end
 
-vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : v:lua.CheckBackSpace() ? "\\<Tab>": coc#refresh()', { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true })
-vim.api.nvim_set_keymap("i", "<S-Tab>", 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"<Paste>', { expr = true })
-vim.api.nvim_set_keymap("i", "<cr>", 'pumvisible() ? "\\<C-y>" : "\\<C-g>u\\<CR>"', { expr = true })
-vim.api.nvim_set_keymap("i", "<C-space>", 'coc#refresh()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : v:lua.CheckBackSpace() ? "\\<Tab>": coc#refresh()', { silent = true, expr = true, noremap = true})
+vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true, noremap = true })
+vim.api.nvim_set_keymap("i", "<S-Tab>", 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', { expr = true, noremap = true })
+vim.api.nvim_set_keymap("i", "<cr>", 'pumvisible() ? "\\<C-y>" : "\\<C-g>u\\<CR>"', { expr = true, noremap = true })
+vim.api.nvim_set_keymap("i", "<c-space>", 'coc#refresh()', { silent = true, expr = true, noremap = true })
 
 vim.api.nvim_set_keymap("n", "<leader>a", "<Plug>(coc-codeaction-selected)", {})
 vim.api.nvim_set_keymap("v", "<leader>a", "<Plug>(coc-codeaction-selected)", {})
@@ -98,46 +96,40 @@ vim.api.nvim_set_keymap("n", "<leader>4", "4gt", {})
 vim.api.nvim_set_keymap("n", "<leader>5", "5gt", {})
 vim.api.nvim_set_keymap("n", "<leader>6", "6gt", {})
 
-vim.g.lightline = {
-  active = {
-    left = { {"mode", "paste"}, { "gitbranch", "readonly", "filename", "modified" } }
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = false,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
   },
   tabline = {
-    left = { { "statuslinetabs" } }
+    lualine_a = {{'tabs', mode = 2}},
   },
-  bufferline = {
-    { show_number = 1, enable_devicons = 1 }
-  },
-  component_expand = {
-    buffers = 'lightline#bufferline#buffers',
-    statuslinetabs = 'LightlineStatuslineTabs'
-  },
-  component_function = {
-    gitbranch = 'FugitiveHead'
-  },
-  component_type = {
-    buffers = 'tabsel'
-  },
-  component = {
-    filename = '%F'
-  }
+  extensions = {}
 }
-
-function _G.LightlineStatuslineTabs()
-  local data = {}
-  for i = 1, table.getn(nvim.api.nvim_list_tabpages()) do
-    local filename = vim.fn['lightline#tab#filename'](i)
-    local current_file = ""
-    if i == nvim.api.nvim_get_current_tabpage() then
-      current_file = "[*] "
-    else
-      current_file = ""
-    end
-    local new_str = current_file .. filename
-    data.insert(new_str)
-  end
-  return table.concat(data, " \\u2b81 ")
-end
 
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep", { noremap = true })
+
+require('gitsigns').setup()
